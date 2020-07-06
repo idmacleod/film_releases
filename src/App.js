d02310ac7e91826ea81f5f66473f5c4e77a68009
idmacleod <idmacleod@gmail.com>
2020-07-06 16:51:27 +0100

    Added UpcomingFilms containeer with state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import UpcomingFilms from './containers/UpcomingFilms';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <UpcomingFilms />
     </div>
   );
 }
